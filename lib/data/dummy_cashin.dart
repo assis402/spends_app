a244d46a694be200b2fa1f88404d7cec1c721852
Matheus de Assis <assis4002@gmail.com>
2021-04-18 21:27:15 -0300

    first commit

---
+++ lib/data/dummy_cashin.dart
@@ -0,0 +1,22 @@
+import 'package:spends_app/models/cashin.dart';
+
+const DUMMY_CASHIN = {
+  '1': const CashIn(
+    id: '1',
+    title: 'Salário',
+    value: 1400.00,
+    description: '',
+  ),
+  '2': const CashIn(
+    id: '2',
+    title: 'Loja',
+    value: 500.00,
+    description: 'Rendimentos da minha loja online',
+  ),
+  '3': const CashIn(
+    id: '3',
+    title: 'Youtube',
+    value: 100.00,
+    description: 'Rendimentos Canal no Youtube',
+  ),
+};
